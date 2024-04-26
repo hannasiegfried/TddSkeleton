00274b57cdee41c926578c41ce0da9b49c480218
Hanna <hanna.siegfried@kellogg.ox.ac.uk>
2024-04-26 10:26:36 +0100

    passes some tests

---
+++ src/main/java/com/oocode/RecentlyUsedList.java
@@ -0,0 +1,24 @@
+package com.oocode;
+
+import java.util.ArrayList;
+
+public class RecentlyUsedList {
+
+    private ArrayList<String> list = new ArrayList<String>();
+
+    public String retrieve(){
+        int defaultRetrieved = 0;
+        return this.retrieve(defaultRetrieved);
+    }
+
+    public void add(String number){
+        this.list.add(number);
+    }
+
+    public String retrieve(int toRetrieve){
+        if (toRetrieve + 1> list.size()){
+            return "";
+        }
+        return this.list.get(toRetrieve);
+    }
+}
--- src/test/java/com/oocode/ExampleTest.java
+++ src/test/java/com/oocode/ExampleTest.java
@@ -2,12 +2,33 @@ package com.oocode;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ExampleTest {
     @Test
-    public void canAnswerTheUniversalQuestion() {
-        assertThat(new Example().answer(), equalTo(42));
+    public void retrieveEmptyList() {
+
+        assertThat(new RecentlyUsedList().retrieve(), equalTo(""));
     }
+    @Test
+    public void retrieveOneNumberStored() {
+        String number = "12345";
+        RecentlyUsedList myList = new RecentlyUsedList();
+        myList.add(number);
+
+        assertThat(myList.retrieve(), equalTo(number));
+    }
+
+    @Test
+    public void retrieveEmptySecondNumberStored() {
+        String number = "12345";
+        RecentlyUsedList myList = new RecentlyUsedList();
+        myList.add(number);
+
+        assertThat(myList.retrieve(1), equalTo(""));
+    }
+
 }
